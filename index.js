c235ef7f72159328945bab49964b04026ba5c7cb
Violet Faye McClure <trm@Timothys-iMac.attlocal.net>
2018-07-07 17:24:08 -0500

    added getquote function

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ let T = new Twit ({
 const quoteApiUrl = "https://api.forismatic.com/api/1.0/?method=getQuote&key=123456&format=json&lang=en";
 
 function getQuote(callback) {
-
+    request(quoteApiUrl, function(error, response, body) {
+        console.log("error:", error); //Print error if there is one
+        console.log("statusCode:", response && response.statusCode); //Print status code if response was received
+        console.log("body:", body);
+        callback(body);
+    });
 }
