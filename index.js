0674371715adf62cabd0be51b4e3b995c267ce72
Violet Faye McClure <trm@Timothys-iMac.attlocal.net>
2018-07-07 17:00:42 -0500

    adding quote

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 let Twit = require("twit");
 let request = require("requiest");
+let secrets = require("./secrets");
 
+let T = new Twit ({
+    consumer_key:  secrets.CONSUMER_KEY,
+    consumer_secret: secrets.CONSUMER_SECRET,
+    access_token: secrets.ACCESS_TOKEN,
+    access_token_secret: secrets.ACCESS_SECRET
+});
+
+const quoteApiUrl = "https://api.forismatic.com/api/1.0/?method=getQuote&key=123456&format=json&lang=en";
+
+function getQuote(callback) {
+
+}
